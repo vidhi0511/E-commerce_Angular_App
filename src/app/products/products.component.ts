ab4c62c7439a9a8f7b05028ca9af127cef508fe8
Vidhi Singh <vidhisingh@Vidhis-MacBook-Pro.local>
2023-02-10 23:19:25 +0530

    initial commit

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,22 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../service/api.service';
+import { CartService } from '../service/cart.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent implements OnInit{
-  //prperty for storing all the product lists
-  // public productList : any;
+export class ProductsComponent implements OnInit {
 
-  // constructor(private api: ApiService) { }
+  public productList : any ;
+  
+  constructor(private api : ApiService, private cartService : CartService) { }
 
-  // ngOnInit(): void {
-  //     this.api.getProduct()
-  //     .subscribe(res=>{
-  //       this.productList = res;
-  //     })
+  ngOnInit(): void {
+    this.api.getProduct()
+    .subscribe(res=>{
+      this.productList = res;
+     
+      this.productList.forEach((a:any) => {
+        if(a.category ==="women's clothing" || a.category ==="men's clothing"){
+          a.category ="fashion"
+        }
+        Object.assign(a,{quantity:1,total:a.price});
+      });
+      console.log(this.productList);
+    });
 
-  // }
+    
+  }
+  addtocart(item: any){
+    this.cartService.addtoCart(item);
+  }
+  
+
 }
--- src/app/service/cart.service.ts
+++ src/app/service/cart.service.ts
@@ -8,16 +8,17 @@ export class CartService {
 
   public cartItemList : any=[]
   public productList = new BehaviorSubject<any>([]);
-
+  public search = new BehaviorSubject<string>("");
   constructor() { }
   getProducts(){
     return this.productList.asObservable();
   }
-  getTotalPrice(){
+  getTotalPrice() : number{
     let grandtotal =0;
     this.cartItemList.map((a:any)=>{
       grandtotal += a.total;
     })
+    return grandtotal;
   }
   addtoCart(product:any){
     this.cartItemList.push(product);
@@ -26,7 +27,15 @@ export class CartService {
   }
   removeCartItem(product:any){
     this.cartItemList.map((a:any,index:any)=>{
-      if(product.id===a.id)
+      if(product.id===a.id){
+        //to remove one data from the array
+        this.cartItemList.splice(index,1);
+      }
     })
+    this.productList.next(this.cartItemList);
+  }
+  removeAllCart(){
+    this.cartItemList = []
+    this.productList.next(this.cartItemList);
   }
 }
